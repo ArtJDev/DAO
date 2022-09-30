003cb55a6e1fded88b2b89fda7f82a4d297508fd
ArtJDev <artemkurilenko1983@yandex.ru>
2022-09-30 10:31:46 +0300

    Переключено на базу данных MySQL

--- src/main/resources/mysqlscript.sql
+++ src/main/resources/mysqlscript.sql
@@ -1,6 +1,4 @@
-select product
-from product p
-         join order_product_list opl on opl.product_id = p.product_id
-         join orders o on o.order_id = opl.order_id
-         join customer c on c.customer_id = o.customer_id
-where c.first_name ilike :name
+select product_name
+from orders o
+         join customers c on o.customer_id = c.id
+where c.name like :name
